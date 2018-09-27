7fe0b889e937514f91ee7c5ff21f8d6ae8690855
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-09-27 10:42:47 -0600

    start game desc

--- Program.cs
+++ Program.cs
@@ -9,7 +9,11 @@ namespace CastleGrimtol
         {
 
             Console.Clear();
-            Console.WriteLine("Turdssssssssssssssss");
+            Console.WriteLine("You have just regained conciousness, you're on a cold wet floor of a castle..");
+            Console.WriteLine("You look around, and as you get up the floor begins to shake!");
+            Console.WriteLine("As you quickly scan the room you notice an endless black hole in the floor to the west, and a cracked open door to the east.");
+            Console.WriteLine("Which direction would you like to go? I'd suggest not falling in the hole...");
+
             Game game = new Game();
             game.StartGame();
         }
