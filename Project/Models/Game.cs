ed1be1f0a0ece0f186e181a46221aad05daf1f12
Rhianon Kilzer <rhianonkilzer@yahoo.com>
2018-09-29 14:37:09 -0600

    roomssetup

--- Project/Models/Game.cs
+++ Project/Models/Game.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 
 namespace CastleGrimtol.Project
@@ -6,14 +7,17 @@ namespace CastleGrimtol.Project
     {
         public Room CurrentRoom { get; set; }
         public Player CurrentPlayer { get; set; }
+        bool playing = false;
+
 
         public void GetUserInput()
         {
-
+            //get that user input stuff
         }
 
         public void Go(string direction)
         {
+            //move the direction to go
 
         }
 
@@ -24,12 +28,12 @@ namespace CastleGrimtol.Project
 
         public void Inventory()
         {
-
+            //Buncha junk in this thing here
         }
 
         public void Look()
         {
-
+            //WHERE?!
         }
 
         public void Quit()
@@ -44,11 +48,32 @@ namespace CastleGrimtol.Project
 
         public void Setup()
         {
+            playing = true;
+            CurrentPlayer = new Player();
+            //setup rooms
+            Room StartingPoint = new Room("Starting Room", "Where you woke up");
+            Room KeyRoom = new Room("The next room through the cracked door", "This room has a shiney key on the floor with a hall leading to the next room");
+            Room LockedRoom = new Room("Locked Room", "This room looks locked...");
+            Room FinalRoom = new Room("The Final Room", "You found the final room!!");
+
+            //setup my exits
+            StartingPoint.Exits.Add("West", KeyRoom);
+            KeyRoom.Exits.Add("South", LockedRoom);
+            LockedRoom.Exits.Add("East", FinalRoom);
+
+
+
 
         }
 
         public void StartGame()
         {
+            Setup();
+            Console.Clear();
+            Console.WriteLine("You have just regained conciousness, you're on a cold wet floor of a castle..");
+            Console.WriteLine("You look around, and as you get up the floor begins to shake!");
+            Console.WriteLine("As you quickly scan the room you notice an endless black hole in the floor to the west, and a cracked open door to the east.");
+            Console.WriteLine("Which direction would you like to go? I'd suggest not falling into the hole...");
 
         }
 
--- Project/Models/Room.cs
+++ Project/Models/Room.cs
@@ -8,5 +8,12 @@ namespace CastleGrimtol.Project
         public string Description { get; set; }
         public List<Item> Items { get; set; }
         public Dictionary<string, IRoom> Exits { get; set; }
+        public Room(string name, string description)
+        {
+            Name = name;
+            Description = description;
+            Exits = new Dictionary<string, IRoom>();
+            Items = new List<Item>();
+        }
     }
 }
